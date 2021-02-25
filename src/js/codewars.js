27279389ab599fb821710af98b8dd755b1884dd9
Artem Smirnov <aratatem2013@gmail>
2021-02-25 10:02:51 +0200

    фікси

--- src/js/codewars.js
+++ src/js/codewars.js
@@ -1927,14 +1927,34 @@ Ingredients, which don't have to be added to the mixture (missing amount =
 //   //   i;
 //   // });
 // }
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  get info() {
-    return '${this.name} age is ${this.age}';
-  }
-}
-var john = new Person('john', 34);
-Test.assertEquals(john.info, 'johns age is 34');
+
+//========================================================
+
+/**FizzBuzz is often one of the first programming puzzles people learn. Now undo it with 
+ * reverse FizzBuzz!
+
+Write a function that accepts a string, which will always be a valid section of FizzBuzz. 
+Your function must return an array that contains the numbers in order to generate the given 
+section of FizzBuzz.
+
+Notes:
+
+If the sequence can appear multiple times within FizzBuzz, return the numbers that generate
+ the first instance of that sequence.
+All numbers in the sequence will be greater than zero.
+You will never receive an empty sequence.
+Examples */
+
+//незавершено!!!!!!!!!!!!!!
+
+// function reverseFizzBuzz(s) {
+//   const arr = s.split(' ');
+
+//   if (arr.indexOf('Fizz') === 0) {
+//     arr[0] = String(arr[1] - 1);
+//   }
+//   arr[arr.indexOf('Fizz')] = String(Number(arr[arr.indexOf('Fizz') - 1]) + 1);
+//   console.log(arr);
+// }
+// console.log(reverseFizzBuzz('1 2 Fizz 4 Buzz')); // [1, 2, 3, 4, 5]
+// console.log(reverseFizzBuzz('Fizz 688 689 FizzBuzz')); // [687, 688, 689, 690]
